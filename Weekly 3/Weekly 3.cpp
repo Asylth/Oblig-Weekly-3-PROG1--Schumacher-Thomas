7ac28318b6d1bb8a05c5a1b27bacbad656a4e032
Asylth <44904158+Asylth@users.noreply.github.com>
2020-09-15 08:25:45 +0200

    Progress

--- Weekly 3/Weekly 3.cpp
+++ Weekly 3/Weekly 3.cpp
@@ -2,10 +2,16 @@
 #include <cstdlib>
 #include <ctime>
 
+void rightwrong();
+void continuegame();
+
 int toguess;
 int guess;
-bool cont = true;
+char yesno;
+bool contrw = true;
 bool run = true;
+int globalcounter;
+
 
 
 //make a guess a number game
@@ -16,8 +22,7 @@ int main()
 		std::srand(std::time(NULL));
 		toguess = rand() % 10 + 1;
 
-		std::cout << "Try to guess my number: ";
-		std::cin >> guess;
+		rightwrong();
 
 	};
 
@@ -26,14 +31,44 @@ int main()
 void rightwrong() {
 
 	do {
-
-		for (int guesscount = 0; ++guesscount;) {
-			std::cout << "Nr of Guesses: " << guesscount << std::endl;
+		for (globalcounter = 0; globalcounter++;) {
+			
 		}
 
+		std::cout << "Try to guess my number: ";
+		std::cin >> guess;
 
+		if (guess == toguess) {
+			std::cout << std::endl << "You guessed the right number! \n";
+			contrw = false;
+			continuegame();
+		}
+		else if (guess < toguess) {
+			std::cout << std::endl << "Your guess is to low \n";
 
-	} while (cont == true);
+		}
+		else if (guess > toguess) {
+			std::cout << std::endl << "Your guess is to high \n";
 
+		}
+		else {//go again
+			std::cout << std::endl << "Enter a valid guess \n";
+		}
+
+	} while (contrw == true);
 }
 
+void continuegame() {
+
+	std::cout << "\n Do you want to continue the game (Y/N): ";
+	std::cin >> yesno;
+
+	switch (yesno) {
+	case 'Y':case 'y':
+		run = true;
+		break;
+	case 'N':case 'n':
+		run = false;
+		break;
+	}
+}
